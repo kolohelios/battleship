36555a47264fa738f72af26361c4b9ebe65e58e0
Jon Edwards <jkedwards@me.com>
2015-04-18 10:57:42 -0700

    add ship placement functionality

--- client/index.js
+++ client/index.js
@@ -1,7 +1,7 @@
 'use strict';
 /* global Firebase */
 
-var root, players, battleships, myKey, myPlayer;
+var root, players, battleships, myKey, myPlayer, lastPaint, rotateCounter;
 
 $(document).ready(init);
 
@@ -13,10 +13,11 @@ function init(){
   $('#login-user').click(loginUser);
   $('#logout-user').click(logoutUser);
   $('#new-player').click(newPlayer);
+  $('#create-and-place-ship').click(placeBattleship);
   players.on('child_added', createPlayer);
   $('#message').text('Ahoy, matey! Ya best be gettin\' to loggin\' if ya wanna play deh game!');
-
-
+  lastPaint = {x: 0, y: 0, vertOrientation: 0};
+  rotateCounter = 0;
 }
 
 function createUser(){
@@ -84,24 +85,76 @@ function createPlayer(snapshot){
   $('#shipcreation').show();
 }
 
-function createBattleship(){
-  var battleshipType = $('#ship-type');
-  var battleshipName = $('#ship-name');
-
-
-}
+// function createBattleship(){
+//   var battleshipType = $('#ship-type');
+//   var battleshipName = $('#ship-name');
+//
+//
+// }
 
 function placeBattleship(){
+  var battleshipType = $('#ship-type').val();
+  console.log(battleshipType);
+  $('#board1 td').on('click', tempPosition);
+}
+
+function tempPosition(event){
+  $('#set-position').on('click', function(){
+    setPosition();
+  });
+
+  var vertOrientation;
+
+  var x = $(this).data('x');
+  var y = $(this).data('y');
+
+  console.log(rotateCounter);
+  if(x === lastPaint.x && y === lastPaint.y){
+    if(rotateCounter === 1){
+      addOrRemoveBattleship('rem', 'dinghy', lastPaint.x, lastPaint.y, lastPaint.vertOrientation, 1);
+      vertOrientation = Math.abs(vertOrientation - 1);
+      addOrRemoveBattleship('add', 'dinghy', x, y, vertOrientation, 1);
+      rotateCounter = 2;
+    } else if(rotateCounter === 2) {
+      addOrRemoveBattleship('rem', 'dinghy', x, y, vertOrientation, 1);
+      rotateCounter = 0;
+    }
+  } else {
+    vertOrientation = 0;
+    addOrRemoveBattleship('rem', 'dinghy', lastPaint.x, lastPaint.y, lastPaint.vertOrientation, 1);
+    addOrRemoveBattleship('add', 'dinghy', x, y, vertOrientation, 1);
+    rotateCounter = 1;
+  }
+  lastPaint = {x: x, y: y, vertOrientation: vertOrientation};
+  console.log(lastPaint);
+}
+
+function setPosition(){
 
 }
 
-function paintBattleship(ship, x, y, orientation){
-
-  var dinghy = ['/assets/dinghy1.png', '/assets/dinghy2.png'];
-
-  for(var i = 0; i < dinghy.length; i++){
-    $('td[data-x="' + x + '"][data-y="' + (y + i) + '"]').append('<img src="/assets/' + ship + i + '.png" height="50" width=50" align="right">').addClass('image').addClass('imagerotate');
+function addOrRemoveBattleship(addOrRemove, shipType, x, y, vertOrientation, board){
+  var ships = [
+    { name: 'dinghy',
+      images: ['/assets/dinghy0.png', '/assets/dinghy1.png']
+    }
+  ];
+  var arridx;
+  ships.forEach(function(ship, i){
+    if(ship.name === shipType){
+      arridx = i;
+    }
+  });
+  var rotateImage = (vertOrientation === 0) ? '' : ' imagerotate';
+  for(var i = 0; i < ships[arridx].images.length; i++){
+    var $loc = $('#board' + board + ' td[data-x="' + x + '"][data-y="' + y + '"]');
+    console.log(ships[arridx].images[i]);
+    if(addOrRemove === 'add'){
+      $loc.append('<img src="' + ships[arridx].images[i] + '" height="50" width=50" align="right">').addClass('image' + rotateImage);
+    }
+    else{
+      $loc.removeClass('image' + rotateImage).find('img').remove();
+    }
+    (vertOrientation === 0) ? x++ : y++;
   }
-
-
 }
