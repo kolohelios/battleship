11569fd0520d7f816d0e48065ca9600d6b1023a0
Jon Edwards <jkedwards@me.com>
2015-04-17 17:47:09 -0700

    add ui and some user handling

--- client/index.js
+++ client/index.js
@@ -1,2 +1,50 @@
 'use strict';
-console.log('apollo -> online');
+/* global Firebase */
+
+var root, players, battleships;
+
+$(document).ready(init);
+
+function init(){
+  root = new Firebase('https://battleshp-kolohelios.firebaseio.com/');
+  players = root.child('players');
+  battleships = root.child('battleships');
+  $('#create-user').click(createUser);
+  $('#login-user').click(loginUser);
+  $('#logout-user').click(logoutUser);
+}
+
+function createUser(){
+  var email = $('#email').val();
+  var password = $('#password').val();
+  root.createUser({
+    email : email,
+    password : password
+  }, function(error, userData) {
+    if (error) {
+      console.log('Error creating user:', error);
+    } else {
+      console.log('Error creating user:', error);
+    }
+  });
+}
+
+function loginUser(){
+  var email = $('#email').val();
+  var password = $('#password').val();
+
+  root.authWithPassword({
+    email : email,
+    password : password
+  }, function(error, authData) {
+    if (error) {
+      console.log('Login Failed!', error);
+    } else {
+      console.log('Login successful');
+    }
+  });
+}
+
+function logoutUser(){
+
+}
