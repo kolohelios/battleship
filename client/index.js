649e4c7457afe0449fc4556736232907c3ced5cc
Jon Edwards <jkedwards@me.com>
2015-04-19 09:25:31 -0700

    add collision detection for initial ship placement

--- client/index.js
+++ client/index.js
@@ -1,9 +1,27 @@
 'use strict';
 /* global Firebase */
 
-var root, players, battleships, games; // Firebase children
+var root, players, battleships, games; // Firebase & children
 var myKey, myPlayer; // Firebase objects and keys
 var lastPaint, rotateCounter, vertOrientation, shipType, x, y; // general globals
+
+var ships = [{
+  name: 'frigate',
+  images: ['/assets/frigate0.png', '/assets/frigate1.png', '/assets/frigate2.png', '/assets/frigate3.png', '/assets/frigate4.png']
+  }, {
+  name: 'corvette',
+  images: ['/assets/corvette0.png', '/assets/corvette1.png', '/assets/corvette2.png', '/assets/corvette3.png']
+  }, {
+  name: 'man-of-war',
+  images: ['/assets/man-of-war0.png', '/assets/man-of-war1.png', '/assets/man-of-war2.png']
+  }, {
+  name: 'junk',
+  images: ['/assets/junk0.png', '/assets/junk1.png', '/assets/junk2.png']
+  }, {
+  name: 'dinghy',
+  images: ['/assets/dinghy0.png', '/assets/dinghy1.png']
+  }
+];
 
 $(document).ready(init);
 
@@ -11,6 +29,7 @@ function init(){
   root = new Firebase('https://battleshp-kolohelios.firebaseio.com/');
   players = root.child('players');
   battleships = root.child('battleships');
+  games = root.child('games');
   $('#create-user').click(createUser);
   $('#login-user').click(loginUser);
   $('#logout-user').click(logoutUser);
@@ -20,6 +39,7 @@ function init(){
   players.on('child_added', createPlayer);
   battleships.on('child_added', displayShip);
   battleships.on('child_changed', updateShip);
+  games.on('child_added', createGame);
   $('#message').text('Ahoy, matey! Ya best be gettin\' to loggin\' if ya wanna play deh game!');
   lastPaint = {x: 0, y: 0, vertOrientation: 0};
   rotateCounter = 0;
@@ -60,7 +80,6 @@ function loginUser(){
       $('#message').text('Arrrr, welcome den. Let\'s get ta work. Add yer plundrun\' ships!');
     }
   });
-
 }
 
 function logoutUser(){
@@ -81,15 +100,24 @@ function newPlayer(){
 }
 
 function createPlayer(snapshot){
-  myPlayer = snapshot.val();
-  myKey = snapshot.key();
-  var avatar = myPlayer.avatar;
-  var handle = myPlayer.handle;
-  var uid = myPlayer.uid;
-  var myUid = root.getAuth().uid;
-  $('#user').hide();
-  $('#charactercreation').hide();
-  $('#shipcreation').show();
+  var player = snapshot.val();
+
+  console.log('player uid: ', player.uid);
+  console.log('root getauth: ', root.getAuth().uid);
+
+  if(root.getAuth().uid === player.uid){
+    myPlayer = snapshot.val();
+    myKey = snapshot.key();
+    var avatar = myPlayer.avatar;
+    var handle = myPlayer.handle;
+    var uid = myPlayer.uid;
+    var myUid = root.getAuth().uid;
+    $('#user').hide();
+    console.log(handle);
+    $('#player1').text(handle);
+    $('#charactercreation').hide();
+    $('#shipcreation').show();
+  }
 }
 
 function placeBattleship(){
@@ -109,9 +137,17 @@ function tempPosition(){
   y = $(this).data('y');
 
   if(rotateCounter === 0){
-    addOrRemoveBattleship('add', shipType, x, y, vertOrientation, 1);
-    rotateCounter = 1;
-    lastPaint = {x: x, y: y, vertOrientation: vertOrientation};
+    var spaceClear = true;
+    getShipCoords(shipType, x, y).forEach(function(loc){
+      if(isShipPresent(loc[0], loc[1], 1) === true){
+        spaceClear = false;
+      }
+    });
+    if(spaceClear){
+      addOrRemoveBattleship('add', shipType, x, y, vertOrientation, 1);
+      rotateCounter = 1;
+      lastPaint = {x: x, y: y, vertOrientation: vertOrientation};
+    }
   }
   else if(rotateCounter === 1){
     addOrRemoveBattleship('rem', shipType, lastPaint.x, lastPaint.y, lastPaint.vertOrientation, 1);
@@ -131,8 +167,6 @@ function setPosition(){
   $('#create-and-place-ship').show();
   $('#set-position').hide();
   $('#board1 td').off('click', tempPosition);
-  rotateCounter = 0;
-  vertOrientation = 0;
   var battleship = {
     shipType: shipType,
     x: x,
@@ -140,42 +174,27 @@ function setPosition(){
     vertOrientation: vertOrientation,
     uid: myPlayer.uid
   };
+  rotateCounter = 0;
+  vertOrientation = 0;
   battleships.push(battleship);
 }
 
 function addOrRemoveBattleship(addOrRemove, shipType, x, y, vertOrientation, board){
   //console.log(addOrRemove, shipType, x, y, vertOrientation, board);
-  var ships = [{
-    name: 'frigate',
-    images: ['/assets/frigate0.png', '/assets/frigate1.png', '/assets/frigate2.png', '/assets/frigate3.png', '/assets/frigate4.png']
-    }, {
-    name: 'corvette',
-    images: ['/assets/corvette0.png', '/assets/corvette1.png', '/assets/corvette2.png', '/assets/corvette3.png']
-    }, {
-    name: 'man-of-war',
-    images: ['/assets/man-of-war0.png', '/assets/man-of-war1.png', '/assets/man-of-war2.png']
-    }, {
-    name: 'junk',
-    images: ['/assets/junk0.png', '/assets/junk1.png', '/assets/junk2.png']
-    }, {
-    name: 'dinghy',
-    images: ['/assets/dinghy0.png', '/assets/dinghy1.png']
-    }
-  ];
   var arridx;
   ships.forEach(function(ship, i){
     if(ship.name === shipType){
       arridx = i;
     }
   });
-  var rotateImage = (vertOrientation === 0) ? '' : ' imagerotate';
+  var rotateImage = (vertOrientation === 0) ? '' : ' shiprotate';
   for(var i = 0; i < ships[arridx].images.length; i++){
     var $loc = $('#board' + board + ' td[data-x="' + x + '"][data-y="' + y + '"]');
     if(addOrRemove === 'add'){
-      $loc.append('<img src="' + ships[arridx].images[i] + '" height="50" width=50" align="right">').addClass('image' + rotateImage);
+      $loc.append('<img src="' + ships[arridx].images[i] + '" height="50" width=50" align="right">').addClass('ship' + rotateImage);
     }
     else{
-      $loc.removeClass('image' + rotateImage).find('img').remove();
+      $loc.removeClass('ship' + rotateImage).find('img').remove();
     }
     if (vertOrientation === 0){
       x++;
@@ -185,17 +204,34 @@ function addOrRemoveBattleship(addOrRemove, shipType, x, y, vertOrientation, boa
   }
 }
 
+function getShipCoords(shipType, checkX, checkY){
+  var arrayOfCoords = [];
+  var arridx;
+  ships.forEach(function(ship, i){
+    if(ship.name === shipType){
+      arridx = i;
+    }
+  });
+  for(var i = 0; i < ships[arridx].images.length; i++){
+    arrayOfCoords.push([checkX, checkY]);
+  }
+  if(vertOrientation === 0){
+    checkX++;
+  } else {
+    checkY++;
+  }
+  return arrayOfCoords;
+}
+
 function displayShip(snapshot){
   var ship = snapshot.val();
-
 
   if(ship.uid === myPlayer.uid){
     addOrRemoveBattleship('add', ship.shipType, ship.x, ship.y, ship.vertOrientation, 1);
     $('#ship-type option[value="' + ship.shipType + '"]').remove();
   }
   if($('#ship-type').find('option').length === 0){
-    $('#message').text('Waiting for another pirate to scrum with!');
-    $('#shipcreation').hide();
+    games.push({ player: [myPlayer.uid] });
   }
 }
 
@@ -204,21 +240,22 @@ function updateShip(snapshot){
 }
 
 function createGame(snapshot){
-  var game = snapshot.val();
-  if(game.players.indexOf(myPlayer.uid) > -1){
+  var gameKey = snapshot.key();
+  players.child(myKey).update({
+    activeGame: gameKey
+  });
+  $('#message').text('Waiting for another pirate to scrum with!');
+  $('#shipcreation').hide();
+  $('#board1').hide();
+  $('#board2').show();
+}
 
-    var myGame = snapshot.val();
-    var myGameKey = snapshot.key();
-    console.log(myGame);
-    console.log(myGameKey);
-
-    console.log(snapshot.val());
-    players.child(myKey).update({
-      activeGame: myGameKey
-    });
-    $('#ship-type').hide();
-    $('#create-and-place-ship').hide();
-
-    $('#resign').show();
+function isShipPresent(x, y, board){
+  var $loc = $('#board' + board + ' td[data-x="' + x + '"][data-y="' + y + '"]');
+  if ($loc.hasClass('ship')){
+    return true;
+  }
+  else{
+    return false;
   }
 }
